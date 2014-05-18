0879a1564d25fbce46af1921907f67b296bac263
danteholm <danteholm@hotmail.com>
2014-05-18 14:04:49 +0200

    first commit

---
+++ Assets/Scripts/camFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camFollow : MonoBehaviour {
+	public Transform target;
+	public float y = 7.0f;
+	public float z = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.position = target.transform.position + new Vector3 (0, y, z);
+	}
+}
