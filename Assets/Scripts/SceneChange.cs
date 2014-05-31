826e831aab8ae351631ca8f3db53f01f66968976
danteholm <danteholm@hotmail.com>
2014-05-31 22:41:43 +0200

    started on sceneChange

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChange : MonoBehaviour {
+	public int lvl;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter (Collider other) {
+		if (other.CompareTag("Player"))
+			Application.LoadLevel (lvl);
+	}
+
+}
